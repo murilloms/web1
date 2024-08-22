f7c223e5827bfe5d0afffe65fc0bbe1412f99c1a
Murillo <129287439+murilloms@users.noreply.github.com>
2024-08-22 10:08:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
 console.log("Script Carregado")
 
-function CalcularAnos() {
-    console.log("Botão de Calcular")
-    let popAinput = document.getElementById("popA").value.trim()
-    let taxaAinput = document.getElementById("taxaA").value.trim()
-    let popBinput = document.getElementById("popB").value.trim()
-    let taxaBinput = document.getElementById("taxaB").value.trim()
+function CalcularTemperatura() {
+    const tempInput = document.getElementById('temp').value;
+    const resultDiv = document.getElementById('resultado');
+    const conversionType = document.querySelector('input[name="conversionType"]:checked');
 
-    if (popAinput === "" || taxaAinput === "" || popBinput === "" || taxaBinput === "") {
-        alert("Por favor, preencha todos os campos")
-        return
+    if (!conversionType) {
+        resultDiv.textContent = 'Selecione um tipo de conversão.';
+        return;
     }
 
-    let popA = parseInt(popAinput)
-    let taxaA = parseInt(taxaAinput) / 100
-    let popB = parseInt(popBinput)
-    let taxaB = parseInt(taxaBinput) / 100
-    let anos = 0
-
-    while (popA < popB) {
-        popA *= (1 + taxaA)
-        popB *= (1 + taxaB)
-        anos++
+    const tempValue = parseFloat(tempInput);
+    if (isNaN(tempValue)) {
+        resultDiv.textContent = 'Digite um valor válido.';
+        return;
     }
 
-    console.log("Anos Calculados", anos)
-    let resultado = document.getElementById("resultado")
-    resultado.innerHTML = `Serão necessários ${anos} anos para que a população do país A ultrapasse ou iguale a população do país B.`
+    let result;
+    if (conversionType.value === 'celsiusToFahrenheit') {
+        result = (tempValue * 9 / 5) + 32;
+        resultDiv.textContent = `${tempValue}° Celsius é igual a ${result.toFixed(2)}° Fahrenheit.`;
+    } else if (conversionType.value === 'fahrenheitToCelsius') {
+        result = (tempValue - 32) * 5 / 9;
+        resultDiv.textContent = `${tempValue}° Fahrenheit é igual a ${result.toFixed(2)}° Celsius.`;
+    }
 }
 
 function LimparCampos() {
-    console.log("Botão de Limpar Campos")
-    document.getElementById("popA").value = ""
-    document.getElementById("taxaA").value = ""
-    document.getElementById("popB").value = ""
-    document.getElementById("taxaB").value = ""
-    let resultado = document.getElementById("resultado")
-    resultado.innerHTML = ""
+    document.getElementById('formTemperatura').reset();
+    document.getElementById('resultado').textContent = '';
 }
-/*innerHTML pode ser usado para receber o conteúdo de um elemento 
-HTML ou para definir um novo conteúdo para ele. Para isso, 
-podemos utilizar a sintaxe do DOM (document. getElement) para 
-realizar essas ações através do JavaScript.*/
